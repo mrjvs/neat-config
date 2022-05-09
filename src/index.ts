46ccd40d797f76868f7413cd2334027a5e4f1ab5
Jelle van Snik <jellevs@gmail.com>
2022-05-09 20:32:12 +0200

    export useful types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+// methods
 export { createConfigLoader } from 'builder/builder';
+
+// types
+export { configBuilder } from 'builder/base';
+export { fragment } from 'loaders/fragment';
+export { ParserTypesType } from 'loaders/file';
+export { dirOptions } from 'loaders/dir';
+export { namingConventionFunc } from 'utils/translators/conventions';
